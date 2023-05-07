6eff289c7db90ba4a7504d9170ef5d41e9768e49
Muhammed Basheer <basheerkomassery@gmail.com>
2023-05-07 10:54:25 +0530

    created view for home page rendering

--- django-basics/first-project/first_project/home_page/views.py
+++ django-basics/first-project/first_project/home_page/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Products
+
+products = Products.objects.all()
 
 # Create your views here.
 
+
 def home_view(request):
-    message =" <h1> Welcome to home page</h1> "
-    return HttpResponse(message)
+    if 'username' in request.session:
+        product = {
+            'products' : products
+        }
+        return render(request,'home_page/index.html',product)
+    return redirect('login_page')
