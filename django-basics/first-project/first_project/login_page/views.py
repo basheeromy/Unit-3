ffd11b9291c57d32cb7c3de392828a75e6e05dda
Muhammed Basheer <basheerkomassery@gmail.com>
2023-05-07 10:59:50 +0530

    created views, urls and forms for user management

--- django-basics/first-project/first_project/login_page/views.py
+++ django-basics/first-project/first_project/login_page/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.contrib import messages
+from .froms import UserRegisterForm
+from django.contrib.auth.models import User
 
 def login_view(request):
+    if 'username' in request.session:
+        return redirect('home_view') 
+    if request.method == 'POST':
+        usernm = request.POST['username']
+        passwd = request.POST['password']
+        """userid = request.POST.get('id',False)
+        print(f"the user id is {userid}")"""
+        user = authenticate(username = usernm,password = passwd)
+        if user is not None:
+            request.session['username'] = usernm
+            return redirect('home_view')
+        else:
+            print("invalid credentials !")
     return render(request,'login_page/login.html')
+
+def sign_up_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            fname = form.cleaned_data.get('first_name')
+            lname = form.cleaned_data.get('last_name')
+            messages.success(request, f"Account created succefully for {fname} {lname}.")
+            return redirect('login_page')
+    else:
+        form = UserRegisterForm()
+        
+    return render(request,'login_page/sign_up.html',{'form':form})
+
+def log_out_view(request):
+    if 'username' in  request.session:
+        request.session.flush()
+    return redirect('login_page')
+    
